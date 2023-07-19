232873d673b2b6e0f7be069abf56c6e086a4c994
Andreas Awouters <awouters.andreas@gmail.com>
2023-07-19 10:56:57 +0200

    color operations

--- a_tracing_lib/src/color.rs
+++ a_tracing_lib/src/color.rs
@@ -16,6 +16,10 @@ impl Color {
     pub const fn new(r: f32, g: f32, b: f32) -> Self {
         Self { r, g, b }
     }
+
+    pub fn lerp(self, rhs: Self, t: f32) -> Self {
+        self + ((rhs - self) * t)
+    }
 }
 
 impl From<Color> for [u8; 4] {
@@ -24,5 +28,77 @@ impl From<Color> for [u8; 4] {
         let g = (value.g.clamp(0.0, 1.0) * 255.0) as u8;
         let b = (value.b.clamp(0.0, 1.0) * 255.0) as u8;
         [r, g, b, 255]
+    }
+}
+
+impl std::ops::Add for Color {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Color::new(self.r + rhs.r, self.g + rhs.g, self.b + rhs.b)
+    }
+}
+
+impl std::ops::AddAssign for Color {
+    fn add_assign(&mut self, rhs: Self) {
+        self.r += rhs.r;
+        self.g += rhs.g;
+        self.b += rhs.b;
+    }
+}
+
+impl std::ops::Sub for Color {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Color::new(self.r - rhs.r, self.g - rhs.g, self.b - rhs.b)
+    }
+}
+
+impl std::ops::SubAssign for Color {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.r -= rhs.r;
+        self.g -= rhs.g;
+        self.b -= rhs.b;
+    }
+}
+
+impl std::ops::Mul<f32> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Color::new(self.r * rhs, self.g * rhs, self.b * rhs)
+    }
+}
+
+impl std::ops::Mul<Color> for f32 {
+    type Output = Color;
+
+    fn mul(self, rhs: Color) -> Self::Output {
+        Color::new(rhs.r * self, rhs.g * self, rhs.b * self)
+    }
+}
+
+impl std::ops::MulAssign<f32> for Color {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.r *= rhs;
+        self.g *= rhs;
+        self.b *= rhs;
+    }
+}
+
+impl std::ops::Div<f32> for Color {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Color::new(self.r / rhs, self.g / rhs, self.b / rhs)
+    }
+}
+
+impl std::ops::DivAssign<f32> for Color {
+    fn div_assign(&mut self, rhs: f32) {
+        self.r /= rhs;
+        self.g /= rhs;
+        self.b /= rhs;
     }
 }
